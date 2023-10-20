05b2ac0c91f863b7b00636dc06242d4160da8b6d
VladCrauciuc <vlad.crauciuc1@gmail.com>
2023-10-20 21:39:55 +0300

    format

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -2,17 +2,17 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 function NewExpense({ onAddExpense }) {
-	function saveExpenseDataHandler(data) {
-		const expenseData = { ...data, id: Math.random().toFixed(3).toString() };
+  function saveExpenseDataHandler(data) {
+    const expenseData = { ...data, id: Math.random().toFixed(3).toString() };
 
-		onAddExpense(expenseData);
-	}
+    onAddExpense(expenseData);
+  }
 
-	return (
-		<div className="new-expense">
-			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-		</div>
-	);
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
 }
 
 export default NewExpense;
