332973461b385f1e7902a721dc2ce71c43cea80d
VladCrauciuc <82878257+VladCrauciuc@users.noreply.github.com>
2023-10-23 11:27:03 +0300

    removed description

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,18 +3,15 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <div className="expense-item__info">
-          <h2>{props.title}</h2>
-          <h4>{props.description}</h4>
-        </div>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
-  );
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date={props.date} />
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
+			</div>
+		</Card>
+	);
 }
 
 export default ExpenseItem;
