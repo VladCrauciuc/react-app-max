c313d6b44114ae7cb55e3ee320578d25b13512d1
VladCrauciuc <82878257+VladCrauciuc@users.noreply.github.com>
2023-10-23 17:09:41 +0300

    *

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 function App() {
 	return (
 		<div>
-			<h1>New course</h1>
+			<header>
+				<img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+				<h1>React Essentials</h1>
+				<p>
+					Fundamental React concepts you will need for almost any app you are
+					going to build!
+				</p>
+			</header>
+			<main>
+				<h2>Time to get started!</h2>
+			</main>
 		</div>
 	);
 }
